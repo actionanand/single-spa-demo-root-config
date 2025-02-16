5f91a87a3adfe816646acdcfcf1d7b8beed99f09
Anand N S <anand.ns@clarivate.com>
2025-02-16 17:56:53 +0530

    lint fix

--- README.md
+++ README.md
@@ -52,7 +52,7 @@ https://cdn.jsdelivr.net/gh/<github-username>/<github-repo-name@branch-name>/<fi
 ## How to build prod build for heroku
 
 ```bash
-$ npx heroku login -i 
+$ npx heroku login -i
 $ npx heroku create ar-single-spa-demo
 $ git push heroku main
 ```
@@ -63,7 +63,6 @@ $ git push heroku main
 ## How It Works
 
 This project uses [single-spa](https://single-spa.js.org/) to architect an app composed of `micro-frontends`. In the root config, the eight other microfrontend apps (such as angular, react, vue, and svelte) are registered with singe-spa. The main `index.ejs` file contains an import map, which references where to find the compiled JavaScript bundle for each microfrontend. [SystemJS](https://github.com/systemjs/systemjs) is the module loader which then loads the bundles when needed. Utility app serves shared data between other apps.
-
 
 ### NB
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
-const { merge } = require("webpack-merge");
-const singleSpaDefaults = require("webpack-config-single-spa");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require('webpack-merge');
+const singleSpaDefaults = require('webpack-config-single-spa');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = (webpackConfigEnv, argv) => {
-  const orgName = "actionanand";
+  const orgName = 'actionanand';
   const defaultConfig = singleSpaDefaults({
     orgName,
-    projectName: "root-config",
+    projectName: 'root-config',
     webpackConfigEnv,
     argv,
     disableHtmlGeneration: true,
@@ -17,11 +17,11 @@ module.exports = (webpackConfigEnv, argv) => {
     plugins: [
       new HtmlWebpackPlugin({
         inject: false,
-        template: "src/index.ejs",
+        template: 'src/index.ejs',
         templateParameters: {
           isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
           orgName,
-          appTitle: "Single Spa Demo",
+          appTitle: 'Single Spa Demo',
           // favicon: "src/assets/favicon.ico",
         },
       }),
